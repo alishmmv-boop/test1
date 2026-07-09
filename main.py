588e65b40466cb82bd1ea7fe88434382ab495ba7
Алишер локал <alishmmv@gmail.com>
2026-07-09 19:01:06 +0700

    add main module test

---
+++ main.py
@@ -0,0 +1,12 @@
+def calculate_average(numbers: list[float]) -> float:
+    if not numbers:
+        return 0.0
+    return sum(numbers) / len(numbers)
+
+def main():
+    data = [10, 20, 30,40, 50]
+    avg = calculate_average(data)
+    print(f"AVG: {avg}")
+
+if __name__ == "__main__":
+    main()
