fd1c7b19eeaf78f2e3b4e6467b7f96f360458941
alishmmv-boop <alishmmv@gmail.com>
2026-07-09 20:05:03 +0700

    add multiply and tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
 import pytest
-from main import calculate_average
+from main import calculate_average, multiply
 
 def test_calculate_average():
     assert calculate_average([10,20,30]) == 20.0
     assert calculate_average([]) == 0.0
     assert calculate_average([5]) == 5.0  
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 5) == -5
